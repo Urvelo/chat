1c4c7f9f2d3d8fa736542a81792b11e7670e52b5
Urvelo <tobuhu07@gmail.com>
2025-09-16 20:47:31 +0000

    SIIVOUS: Poistettu ylimääräiset tiedostot ja optimoitu projekti

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Lataa moderation service globaalisti testejä varten
-import moderationService from './utils/moderation.js'
-window.moderationService = moderationService
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
